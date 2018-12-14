12ac9dca821467c60d589aa3feacc899df0874a5
hmlhml <405185142@qq.com>
2018-12-14 09:23:01 +0800

    second commit

---
+++ src/pages/index.js
@@ -0,0 +1,29 @@
+import login from './login';
+import session from './session';
+import contact from './contact';
+import namecard from './namecard';
+import searchUser from './searchUser';
+import chatroom from './chatroom';
+import general from './general';
+import myinfo from './myinfo';
+import about from './about';
+import chat from './chat';
+import chatHistroy from './chatHistroy';
+import sysmsg from './sysmsg';
+import test from './test';
+
+export default {
+  login,
+  session,
+  contact,
+  namecard,
+  searchUser,
+  chatroom,
+  general,
+  myinfo,
+  about,
+  chat,
+  chatHistroy,
+  sysmsg,
+  test,
+};
